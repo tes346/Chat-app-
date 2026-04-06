e7e2026962dca51f256f6362c7104573e16c7e7a
tes346 <teshjunior438@gmail.com>
2026-04-06 23:18:53 +0300

    Update Firebase config with new credentials
    Updated Firebase configuration with new values.

--- intel.js
+++ intel.js
@@ -1,15 +1,14 @@
 // 1. YOUR FIREBASE CONFIG
-const firebaseConfig = {
-  apiKey: "AIzaSyCkK8FYqYgwcZTgbhEIG-3qOD5BkBL_Qj4",
-  authDomain: "chat-app-72173.firebaseapp.com",
-  projectId: "chat-app-72173",
-  storageBucket: "chat-app-72173.firebasestorage.app",
-  messagingSenderId: "350285511724",
-  appId: "1:350285511724:web:84c0128616b2880313e589",
-  measurementId: "G-RHBBRTWLVY",
-  databaseURL: "https://chat-app-72173-default-rtdb.firebaseio.com"
-};
 
+  const firebaseConfig = {
+    apiKey: "AIzaSyCkK8FYqYgwcZTgbhEIG-3q0D5BkBL_Qj4",
+    authDomain: "chat-app-72173.firebaseapp.com",
+    projectId: "chat-app-72173",
+    storageBucket: "chat-app-72173.firebasestorage.app",
+    messagingSenderId: "350285511724",
+    appId: "1:350285511724:web:a61643a04b5d2d2113e589",
+    measurementId: "G-BTN960VVZ1",
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
